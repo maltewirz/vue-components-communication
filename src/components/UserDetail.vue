3074587663a7164525a2691d0f6dea049f3e0c7f
Malte Wirz <mail@maltewirz.com>
2019-12-08 12:00:20 +0100

    naming

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,13 +2,27 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
-        <p>User Name : {{ name }}</p>
+        <p>User Name : {{ switchName() }}</p>
+        <button @click="resetName">Reset name</button>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['name']
+        props: {
+            myName: String,
+            required: true,
+            // default: "Dude" // this
+        },
+        methods: {
+            switchName() {
+                return this.myName.split("").reverse().join("");
+            },
+            resetName() {
+                this.myName = 'Dude';
+                this.$emit('nameWasReset', this.myName);
+            }
+        }
     }
 </script>
 
